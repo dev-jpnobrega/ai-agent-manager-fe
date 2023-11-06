fdeef206ea65043291b53b09c08d2ca2e36aaccd
Joao Paulo Nobrega <joao.paulo.nobrega@avanade.com>
2023-11-06 12:15:41 -0300

    remove aws cdognito

--- src/index.js
+++ src/index.js
@@ -4,14 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import Amplify from 'aws-amplify';
 import './i18n';
-
-Amplify.configure({
-  aws_cognito_region: "us-east-1", // (required) - Region where Amazon Cognito project was created   
-  aws_user_pools_id:  "us-east-1_jWnKlle4V", // (optional) -  Amazon Cognito User Pool ID   
-  aws_user_pools_web_client_id: "3okmbk5lpiih1rl29ak2qv2uh2", // (optional) - Amazon Cognito App Client ID (App client secret needs to be disabled)
-})
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
